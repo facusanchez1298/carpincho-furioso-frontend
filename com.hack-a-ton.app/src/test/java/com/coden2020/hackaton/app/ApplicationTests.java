eefe2aa804f14a661da8d1a7bd7b8281a3675c03
Deknasis <facux96_sotelo@hotmail.com>
2020-05-02 16:50:54 -0300

    Added all dependencies

---
+++ com.hack-a-ton.app/src/test/java/com/coden2020/hackaton/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coden2020.hackaton.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
